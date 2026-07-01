af6c919b84a1a40ce8cf1b947ffc91d9aefb081a
https://github.com/nethravathity-droid <nethravathity@gmail.com>
2026-07-01 12:59:24 +0530

    Enhance DbBox component to improve cart functionality by integrating AddToCartButton. Update CSS for better layout and styling consistency across product pages.

--- src/components/HeroProductCart.jsx
+++
@@ -1,9 +0,0 @@
-import ProductPageCart from "./ProductPageCart";
-
-export default function HeroProductCart({ productId }) {
-  return (
-    <div className="hero-product-cart">
-      <ProductPageCart productId={productId} />
-    </div>
-  );
-}
